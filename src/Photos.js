29a643942654c2506d483e40fb4b7f943fa58402
Gelarehak <gelareh.akasheh@gmail.com>
2021-12-12 14:33:47 +0330

    photo style

--- src/Photos.js
+++ src/Photos.js
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function Photos(props) {
   if (props.photos) {
+    console.log(props.photos);
     return (
       <section>
         <div className="row">
-          {props.photos.map(function (photos, index) {
+          {props.photos.map(function (photo, index) {
             <div className="col-3" key={index}>
-              <img src={photos.src.landscape} alt="photoWord" />;
+              <img src={photo.src.landscape} alt="photoWord" />;
             </div>;
           })}
         </div>
